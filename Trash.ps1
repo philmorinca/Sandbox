8a28b981a3815f138080f8b47bf1cca2f46278f5
Philippe Morin <philippe.morin.qc.ca@gmail.com>
2022-07-29 16:19:50 -0400

    Initial

---
+++ Trash.ps1
@@ -0,0 +1,8 @@
+﻿$DesktopPath = [Environment]::GetFolderPath("Desktop")
+$mypath ="$DesktopPath\Fichiers piratés"
+
+mkdir $mypath
+Move-Item "$DesktopPath\*" $mypath
+
+$shell = New-Object -ComObject "Shell.Application"
+$shell.minimizeall()
