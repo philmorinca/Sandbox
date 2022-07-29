cb30dcad3f779ac7958403b3173e3482a1957108
philmorinca <philippe.morin.qc.ca@gmail.com>
2022-07-29 17:30:01 -0400

    10x

--- Trash.ps1
+++ Trash.ps1
@@ -1,11 +1,14 @@
 $DesktopPath = [Environment]::GetFolderPath("Desktop")
-$mypath ="$DesktopPath\Fichiers pirates"
-If(!(test-path -PathType container $mypath))
-{
+$mypath = "$DesktopPath\Fichiers pirates"
+If (!(test-path -PathType container $mypath)) {
     mkdir "$mypath"
     Move-Item "$DesktopPath\*" "$mypath"
     Move-Item "$DesktopPath\*.lnk" "$mypath"
     
     $shell = New-Object -ComObject "Shell.Application"
     $shell.minimizeall()
+
+    
 }
+
+for ($i = 1; $i -le 10; $i++) { & calc.exe }
