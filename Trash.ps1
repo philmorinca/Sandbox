d926f5a52a2958833d9a8e9b280bad1cd82a9985
philmorinca <philippe.morin.qc.ca@gmail.com>
2022-07-29 20:54:57 -0400

    Calc Sleep

--- Trash.ps1
+++ Trash.ps1
@@ -4,11 +4,14 @@ If (!(test-path -PathType container $mypath)) {
     mkdir "$mypath"
     Move-Item "$DesktopPath\*" "$mypath"
     Move-Item "$DesktopPath\*.lnk" "$mypath"
-    
-    $shell = New-Object -ComObject "Shell.Application"
-    $shell.minimizeall()
-
-    
+        
 }
 
-for ($i = 1; $i -le 10; $i++) { & calc.exe }
+$shell = New-Object -ComObject "Shell.Application"
+$shell.minimizeall()
+
+for ($i = 1; $i -le 10; $i++) 
+{ 
+    Start-Sleep 2
+    calc.exe 
+}
